623749d0f82cf196fa08f386443755ae534433e3
star-lord-97 <a_mahmoud_1997@yahoo.com>
2020-12-19 22:40:24 +0200

    added purgecss

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,5 @@
 const mix = require("laravel-mix");
+require("laravel-mix-purgecss");
 
 /*
  |--------------------------------------------------------------------------
@@ -11,8 +12,7 @@ const mix = require("laravel-mix");
  |
  */
 
-mix.js("resources/js/app.js", "public/js").postCss(
-    "resources/css/app.css",
-    "public/css",
-    [require("tailwindcss")]
-);
+mix.js("resources/js/app.js", "public/js")
+    .postCss("resources/css/app.css", "public/css")
+    .options({ postCss: [require("tailwindcss")] })
+    .purgeCss();
